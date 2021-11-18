3f7e645d3c47706c413cdd2d19fb6c2366da69e1
Tanirbergen <87113165+Tanirzum@users.noreply.github.com>
2021-11-18 11:46:04 +0600

    finallCommit

---
+++ src/VersionControlRunner.java
@@ -0,0 +1,10 @@
+public class VersionControlRunner {
+    public static void main(String[] args) {
+        Project master = new Project(1, "Super", "SourceCode");
+        System.out.println(master);
+
+        ProjectFactory factory = new ProjectFactory(master);
+        Project masterClone = factory.cloneProject();
+        System.out.println(masterClone);
+    }
+}
